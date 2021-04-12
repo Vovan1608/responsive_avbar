849c0daf7d64a7b377c092ff5a2457a4084795f9
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-12 13:40:13 +0300

    removed if...else, added toggle

--- main.js
+++ main.js
@@ -4,9 +4,5 @@ const menuButton = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 menuButton.addEventListener("click", () => {
-	if (links.classList.contains("show-links")) {
-		links.classList.remove("show-links");
-	} else {
-		links.classList.add("show-links");
-	}
+	links.classList.toggle("show-links");
 });
