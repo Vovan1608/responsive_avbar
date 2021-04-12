535304db5d41cbeb85883e32a67a688a37708ca2
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-12 13:38:30 +0300

    added if...else

--- main.js
+++ main.js
@@ -4,5 +4,9 @@ const menuButton = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 menuButton.addEventListener("click", () => {
-	console.log(links.classList);
+	if (links.classList.contains("show-links")) {
+		links.classList.remove("show-links");
+	} else {
+		links.classList.add("show-links");
+	}
 });
