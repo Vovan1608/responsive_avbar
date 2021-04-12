0ee7b322fdcee999c6835d7c16788984328b9071
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-12 13:32:01 +0300

    added addEventListener to menuButton

--- main.js
+++ main.js
@@ -2,3 +2,7 @@
 
 const menuButton = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+
+menuButton.addEventListener("click", () => {
+	console.log(links.classList);
+});
